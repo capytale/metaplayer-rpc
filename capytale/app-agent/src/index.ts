4681b32d4dafc06fbe2b2462e405ae40fb600684
tjaisson <thomas.jaisson@ac-paris.fr>
2024-04-17 20:14:04 +0000

    Minor

--- capytale/app-agent/src/index.ts
+++ capytale/app-agent/src/index.ts
@@ -8,7 +8,7 @@ let socket: Socket<Contracts, 'application'> | undefined = undefined;
  * @param mpOrigin L'origine du *MetaPlayer*.
  * @returns Le socket de l'*Application*.
  */
-function getSocket(mpOrigin: string): Socket<Contracts, 'application'> {
+function getSocket(mpOrigin?: string): Socket<Contracts, 'application'> {
     if (socket != null) return socket;
     socket = createSocket<Contracts, 'application'>(createApplicationLink(mpOrigin));
     return socket;

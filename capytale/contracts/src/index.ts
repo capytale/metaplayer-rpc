e95e81f61b3b552befa31c4081157699886c4313
tjaisson <thomas.jaisson@ac-paris.fr>
2024-04-18 21:02:03 +0000

    Add a contract from theme management

--- capytale/contracts/src/index.ts
+++ capytale/contracts/src/index.ts
@@ -2,10 +2,17 @@
 import { AddIdData, CollectionOf } from "@capytale/contract-type";
 
 import type { SimpleContentV1 } from "./simple-content";
-
 type SimpleContentContracts = [
     AddIdData<SimpleContentV1<string>, { name: "simple-content", variant: "text" }>,
     AddIdData<SimpleContentV1<any>, { name: "simple-content", variant: "json" }>,
 ];
 
-export type Contracts = CollectionOf<SimpleContentContracts>;
+import { ThemeV1 } from "./theme";
+type ThemeContracts = [
+    AddIdData<ThemeV1, { name: "theme" }>,
+];
+
+export type Contracts =
+    CollectionOf<SimpleContentContracts> &
+    CollectionOf<ThemeContracts>;
+    
---
+++ capytale/contracts/src/theme.ts
@@ -0,0 +1,36 @@
+/**
+ * Ce module définit la transmission du thème d'affichage.
+ */
+
+/**
+ * Un contrat pour gérer un contenu simple :
+ */
+export type ThemeV1 = {
+    version: 1;
+    /**
+     * L'interface qui expose le *MetaPlayer* à l'*Application*.
+    */
+    metaplayer: {
+        /**
+         * L'*Application* peut appeler cette méthode pour connaître le thème actuel.
+         * 
+         * @returns null si le thème n'est pas défini.
+         *          Sinon, le nom du thème actuel 'light' ou 'dark'.
+         */
+        getCurrentTheme(): string | null;
+    };
+
+    /**
+     * L'interface qui expose l'*Application* au *MetaPlayer*.
+     */
+    application: {
+        /**
+         * Le *MetaPlayer* appelle cette méthode pour indiquer un changement de
+         * thème à l'*Application*.
+         * 
+         * @param theme le nouveau thème à appliquer 'light' ou 'dark',
+         *              null signifie que le thème n'est pas défini. 
+        */
+        setTheme(theme: string | null): void;
+    };
+}

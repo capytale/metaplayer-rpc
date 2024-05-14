2ffdf869b94045da9be546a18facb7cc0e799a42
tjaisson <thomas.jaisson@ac-paris.fr>
2024-05-14 16:53:18 +0200

    format

--- contract-type/src/interface.ts
+++ contract-type/src/interface.ts
@@ -2,6 +2,5 @@
  * Un contrat d'interface est un ensemble de fonctions.
  */
 export type ContractInterface = {
-    readonly [key: string]: (...args: any[]) => unknown;
-  };
-  
+  readonly [key: string]: (...args: any[]) => unknown;
+};

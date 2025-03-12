2c17f32815b9b13d5040cd2fb86a9977b901288c
FredZinelli <145555278+FredZinelli@users.noreply.github.com>
2025-03-12 18:10:30 +0100

    Update mode.ts

--- capytale/contracts/src/mode.ts
+++ capytale/contracts/src/mode.ts
@@ -1,5 +1,15 @@
 /**
  * Ce module définit la transmission du mode de l'activité.
+ *
+ * Les modes peuvent être :
+ *    - `"create"` - Pour un enseignant en train de créer/modifier une activité.
+ *    - `"assignment"` - Pour un élève/participant en train d'effectuer le travail demandé.
+ *    - `"review"` - Pour un enseignant regardant la copie d'un élève.participant (que la
+ *                   copie soit finalisée ou non: voir le contrat `workflow`).
+ *    - `"view"` - Pour un enseignant regardant une activité créée par un autre enseignant
+ *                 depuis la bibliothèque d'activité de Capytale.
+ *
+ * Souscription avec `mode:{v}`, où `{v}` est le numéro de version du contrat.
  */
 
 type Mode =
@@ -32,11 +42,10 @@ export type ModeV1 = {
      */
     application: {
         /**
-         * Le *MetaPlayer* appelle cette méthode pour indiquer le mode à l'*Application*,
-         * après avoir appelé la méthode `loadContent`.
-         * Ne devrait être appelé qu'une seule fois.
+         * Le *MetaPlayer* appelle systématiquement cette méthode pour indiquer le mode à l'*Application*
+         * avant d'avoir appelé la méthode `loadContent` des contrats `simple-content`.
          * 
-         * @param mode le mode à appliquer. 
+         * @param mode le mode utilisé actuellement pour l'activité. 
         */
         setMode(mode: Mode): void;
     };

1187fac5a584d948fc6e00eb2788333f6516812e
tjaisson <thomas.jaisson@ac-paris.fr>
2025-09-04 14:41:34 +0200

    Revert "Remove unused contract"
    This reverts commit 570d91ceb9ca1dcd1a0d2e7b181acaf502505360.

--- capytale/contracts/src/index.ts
+++ capytale/contracts/src/index.ts
@@ -5,6 +5,7 @@ import type { SimpleContentV1 } from "./simple-content";
 type SimpleContentContracts = [
     AddIdData<SimpleContentV1<string>, { name: "simple-content", variant: "text" }>,
     AddIdData<SimpleContentV1<any>, { name: "simple-content", variant: "json" }>,
+    AddIdData<SimpleContentV1<any>, { name: "simple-binary-data", variant: "text" }>,
 ];
 
 import type { ThemeV1 } from "./theme";
